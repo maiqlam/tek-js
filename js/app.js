91ac85b64afdc6fcc196b7f7ea47d7dbf595a429
maiqlam <maiqlam@gmail.com>
2023-05-22 12:05:12 -0500

    console log comments data types

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// console.log("hello world");
+
+// console.log("js test");
+
+let num = 5;
+
+let name = 'john';
+
+let isMonday = true;
+
+let name2 = null;
+
+let name3 = undefined;
+
+let student = {
+    name:"Mai",
+    age: 29,
+};
